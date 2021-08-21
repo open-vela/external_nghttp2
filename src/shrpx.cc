0cd8356fdea378f641af4e1bc8c411ac35829513
openvela-robot <openvela-robot@xiaomi.com>
2021-08-21 19:00:43 +0800

    Build without HTTP/3 support

--- src/shrpx.cc
+++ src/shrpx.cc
@@ -1548,6 +1548,21 @@ void fill_default_config(Config *config) {
         downstreamconf.option, downstreamconf.encoder_dynamic_table_size);
   }
 
+#ifdef ENABLE_HTTP3
+  auto &quicconf = config->quic;
+  {
+    quicconf.timeout.idle = 30_s;
+
+    auto &stateless_resetconf = quicconf.stateless_reset;
+    // TODO Find better place to do this and error handling.
+    if (RAND_bytes(stateless_resetconf.secret.data(),
+                   stateless_resetconf.secret.size()) != 1) {
+      LOG(FATAL) << "Unable to generate stateless reset secret";
+      exit(EXIT_FAILURE);
+    }
+  }
+#endif // ENABLE_HTTP3
+
   auto &loggingconf = config->logging;
   {
     auto &accessconf = loggingconf.access;

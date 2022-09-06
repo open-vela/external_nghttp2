217fe872fed768612583b70263ac5b5b8eb87ad3
liujinye <liujinye@xiaomi.com>
2022-09-06 20:55:12 +0800

    Merge pull request #1791 from nghttp2/bump-ngtcp2
    Bump ngtcp2

--- src/http2.cc
+++ src/http2.cc
@@ -693,17 +693,6 @@ StringRef rewrite_location_uri(BlockAllocator &balloc, const StringRef &uri,
   return StringRef{iov.base, p};
 }
 
-int check_nv(const uint8_t *name, size_t namelen, const uint8_t *value,
-             size_t valuelen) {
-  if (!nghttp2_check_header_name(name, namelen)) {
-    return 0;
-  }
-  if (!nghttp2_check_header_value(value, valuelen)) {
-    return 0;
-  }
-  return 1;
-}
-
 int parse_http_status_code(const StringRef &src) {
   if (src.size() != 3) {
     return -1;
@@ -1816,6 +1805,53 @@ StringRef normalize_path(BlockAllocator &balloc, const StringRef &path,
         auto c =
             (util::hex_to_uint(*(it + 1)) << 4) + util::hex_to_uint(*(it + 2));
         if (util::in_rfc3986_unreserved_chars(c)) {
+          *p++ = c;
+
+          it += 3;
+
+          continue;
+        }
+        *p++ = '%';
+        *p++ = util::upcase(*(it + 1));
+        *p++ = util::upcase(*(it + 2));
+
+        it += 3;
+
+        continue;
+      }
+    }
+    *p++ = *it++;
+  }
+
+  p = std::copy(it, std::end(path), p);
+  *p = '\0';
+
+  return path_join(balloc, StringRef{}, StringRef{}, StringRef{result.base, p},
+                   query);
+}
+
+StringRef normalize_path_colon(BlockAllocator &balloc, const StringRef &path,
+                               const StringRef &query) {
+  // First, decode %XX for unreserved characters and ':', then do
+  // http2::path_join
+
+  // We won't find %XX if length is less than 3.
+  if (path.size() < 3 ||
+      std::find(std::begin(path), std::end(path), '%') == std::end(path)) {
+    return path_join(balloc, StringRef{}, StringRef{}, path, query);
+  }
+
+  // includes last terminal NULL.
+  auto result = make_byte_ref(balloc, path.size() + 1);
+  auto p = result.base;
+
+  auto it = std::begin(path);
+  for (; it + 2 < std::end(path);) {
+    if (*it == '%') {
+      if (util::is_hex_digit(*(it + 1)) && util::is_hex_digit(*(it + 2))) {
+        auto c =
+            (util::hex_to_uint(*(it + 1)) << 4) + util::hex_to_uint(*(it + 2));
+        if (util::in_rfc3986_unreserved_chars(c) || c == ':') {
           *p++ = c;
 
           it += 3;
